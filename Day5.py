800342f3bdbc54a932b50299add7f74ae0460bb2
Naif Alqahtani <50847406+NaifAlqahtani@users.noreply.github.com>
2019-08-20 22:30:08 +0300

    Day5.py

--- Day5.py
+++ Day5.py
@@ -6,4 +6,19 @@ basket = x + y + z
 
 
 for i in range(0,len(basket),5): 
-    print(basket[i:i+5], end = ' ')
+    print(basket[i:i+5], end = ' ')  #I tried to split the string every 5 letters, unfortunatly orange 
+                                     #is 6 letters so the output will be:
+                                     # >>>> apple orang elemo n
+                                     #
+                                     #if orange was replaced with 'grape', the output will be:
+                                     # >>>> apple grape lemon
+                        
+                        
+#an easier way to do this correctly will be as follows:
+"""
+x= "apple"
+y = "orange"
+z= "lemon"
+
+print(x,y,z)
+"""
