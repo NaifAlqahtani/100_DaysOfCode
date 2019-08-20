abff35029755e1308b18c0a9e4ce70aaca073703
NaifAlqahtani <naiftheprince@gmail.com>
2019-08-20 22:24:34 +0300

    New

---
+++ Day4.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(4): #I ran the example 4 times.
+    randomNumber = random.randint(1,10)
+    print(randomNumber)
---
+++ Day5.py
@@ -0,0 +1,9 @@
+x= "apple"
+y = "orange"
+z= "lemon"
+
+basket = x + y + z
+
+
+for i in range(0,len(basket),5): 
+    print(basket[i:i+5], end = ' ')
