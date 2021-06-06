aad8218e43d1cbb3b3b84bc2d083cbf12093c672
NaifAlqahtani <naiftheprince@gmail.com>
2021-06-06 11:30:48 +0300

    day1 flutter

---
+++ 100 days of flutter/day1/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/naif/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/naif/Documents/100 days of code/100_DaysOfCode/100 days of flutter/day1"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
